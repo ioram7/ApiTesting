9be2b23c4a4546293e7d140f5c06466cda592b47
chaves <mauricio.chaves.junior@live.com>
2019-04-06 14:25:14 -0300

    add upload_file

---
+++ features/upload_file.py
@@ -0,0 +1,10 @@
+import dropbox
+
+
+file_name='deploy.zip'
+dropbox_path='/var/jenkins_home/workspace/pipeline@2/'
+dbx=dropbox.Dropbox('KY62MTunDIAAAAAAAAAADNLoux_cL3txFSrc25YVZpUp6TIpcT0psYR9aqNsyomR')
+
+
+with open(file_name, 'rb') as f:
+    dbx.files_upload(f.read(),dropbox_path+file_name,mute=True)
